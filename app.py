9c39cc1139c42a1ff9854fb7085fec093e04f033
deepshika21 <deepshikamaheshwaram21@gmail.com>
2025-11-30 23:57:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import json
 import os
 
 # ================= CONFIG =================
-N8N_WEBHOOK_URL = "https://deepshika021.app.n8n.cloud/webhook/eli5"
+N8N_WEBHOOK_URL = "https://deepshika021.app.n8n.cloud/webhook/Understand easily"
 DATA_FILE = "chats.json"
 # ==========================================
 
@@ -212,4 +212,5 @@ if user_input:
 
 # ---------- FOOTER ----------
 st.markdown("<p class='caption'>Built to understand, not memorise.</p>", unsafe_allow_html=True)
+
 
